2d15f88c9ab75db24fa4ef601425a04791c84df8
Blaine <blaine.prestenbach@selu.edu>
2021-12-02 20:46:13 -0600

    create-react-app

---
+++ cyber-security/src/App.js
@@ -0,0 +1,13 @@
+import bottomlessPit from './Images/bottomlessPit.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={bottomlessPit} alt="logo" width="400" height="400"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
