89adcf1f1a44e8cb311b9ab6a2344753b146fe22
Blaine <blaine.prestenbach@selu.edu>
2021-12-08 21:49:24 -0600

    fix: image sizing

--- cyber-security/src/Components/MainCarousel.js
+++ cyber-security/src/Components/MainCarousel.js
@@ -1,27 +1,28 @@
 import { Carousel } from 'react-bootstrap';
 import serverBros from '../Images/serverBros.jpg'
+import CSIT_building from '../Images/CSIT_building.jpg'
 
 export default function MainCarousel() {
   return (
-    <div style={{backgroundColor: 'black'}}>
+    <div style={{backgroundColor: 'black', maxWidth: 1000, maxHeight: 1000, margin: 'auto'}}>
       <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              style={{opacity: 0.5}}
-              src={serverBros}
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
             style={{opacity: 0.5}}
             src={serverBros}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            style={{opacity: 0.5}}
+            src={CSIT_building}
             alt="Second slide"
           />
           <Carousel.Caption>
