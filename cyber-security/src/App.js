6b694ba65f30285990b214f65a57810afb611f98
Tyler Daspit <54476633+tdaspit@users.noreply.github.com>
2021-12-08 19:35:02 -0600

    information-text

--- cyber-security/src/App.js
+++ cyber-security/src/App.js
@@ -1,4 +1,5 @@
 import 'react-bootstrap';
+import React, {useState} from 'react';
 import './index.css';
 import bottomlessPit from './Images/bottomlessPit.jpg';
 import logo from './Images/logo.png';
@@ -47,7 +48,20 @@ function App() {
       )
 })}
 
+<h1 className="center">Where SELU Students go to Advance their skills in Information Security.</h1>
 
+<h1 className="center">GOAL</h1>
+<h4 className="center">To create a space for Southeastern students to freely and supportively cultivate their cybersecurity skills and apply those skills to specific topics and fields that interest them, in an environment that is beginner-friendly while also challenging for advanced students.</h4>
+
+<h1 className="center">ABOUT</h1>
+<h4 className="center">The SELU Cybersecurity Group is a student-run and faculty-supported group operating under the umbrella of the Southeastern chapter of the Association of Computing Machinery. By offering a variety of events, workshops, and social events, we endeavor to inspire growth and help form competent cybersecurity professionals.</h4>
+
+
+<h1 className="center">EVENTS Workshops:</h1>
+<h4 className="center">By leveraging the community of SELU graduates and through faculty networking, we offer focused, pinpoint workshops on specific cybersecurity topics to assist students in applying security to their particular interests.</h4>
+
+<h1 className="center">EVENTS Competitions</h1>
+<h4 className="center">We use many different resources to create competitive events where students (both beginner and expert) can practice their skills against each other in a variety of environments.</h4>
 </div>
   );
 }
