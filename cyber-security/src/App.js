2bd357bffea31ebc65b6ee5e2824d0d455fd3457
Blaine <blaine.prestenbach@selu.edu>
2021-12-08 19:46:39 -0600

    added basic carousel

--- cyber-security/src/App.js
+++ cyber-security/src/App.js
@@ -5,50 +5,37 @@ import logo from './Images/logo.png';
 import serverBros from './Images/serverBros.jpg';
 import ImageSlider from './ImageSlider';
 import { SliderData } from './SliderData';
-
+import MainCarousel from './Components/MainCarousel'
+import { Carousel } from 'react-bootstrap';
 
 function App() {
   return (
     <div>
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      
-  <a class="navbar-brand" href="#"><img src={logo} className="logo"/>Southeastern's Cyber Security Group</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarText">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Join</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Events</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About Us</a>
-      </li>
-    </ul>
-    <span class="navbar-text">
-      Navbar text with an inline element
-    </span>
-  </div>
-</nav>
-
-<img src={serverBros} className="center"/>
-
-<ImageSlider />
-{SliderData.map((slide, index) =>{
-      return (
-        <img src={slide.image} alt='cybersecurity-images'></img>
-      )
-})}
-
-
-</div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"><img src={logo} className="logo"/>Southeastern's Cyber Security Group</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Join</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Events</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About Us</a>
+            </li>
+          </ul>
+          <span class="navbar-text">Navbar text with an inline element</span>
+        </div>
+      </nav>
+      <MainCarousel />
+    </div>
   );
 }
 
